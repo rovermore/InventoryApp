67c2dbf47946da36341b5e7c6f4c64eb1cdc6556
rovermore <robertomoreno@MacBook-Pro.local>
2018-09-01 18:39:17 +0200

    App finished

--- app/src/main/java/com/example/rovermore/inventoryapp/ItemCursorAdapter.java
+++ app/src/main/java/com/example/rovermore/inventoryapp/ItemCursorAdapter.java
@@ -25,20 +25,9 @@ public class ItemCursorAdapter extends CursorAdapter {
     }
 
     @Override
-    public View newView(final Context context, final Cursor cursor, ViewGroup parent) {
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
 
         View row = LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
-
-        Button saleButton = (Button)row.findViewById(R.id.sale_button);
-
-        saleButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                sellProduct(cursor, context);
-
-            }
-        });
 
         return row;
     }
@@ -49,7 +38,7 @@ public class ItemCursorAdapter extends CursorAdapter {
     }
 
     @Override
-    public void bindView(View view, final Context context, Cursor cursor) {
+    public void bindView(View view, final Context context, final Cursor cursor) {
 
         //set views for name quantity and price
         TextView name = (TextView) view.findViewById(R.id.product_name);
@@ -57,7 +46,7 @@ public class ItemCursorAdapter extends CursorAdapter {
         TextView price = (TextView) view.findViewById(R.id.price);
         //get values from cursor an save in variables
         String mName = cursor.getString(cursor.getColumnIndexOrThrow(ItemContract.ItemEntry.PRODUCT_NAME));
-        String mQuantity = String.valueOf(cursor.getInt(cursor.getColumnIndexOrThrow(ItemContract.ItemEntry.QUANTITY)));
+        final String mQuantity = String.valueOf(cursor.getInt(cursor.getColumnIndexOrThrow(ItemContract.ItemEntry.QUANTITY)));
         String mPrice = String.valueOf(cursor.getInt(cursor.getColumnIndexOrThrow(ItemContract.ItemEntry.PRICE)));
 
         //set values to the views
@@ -68,15 +57,25 @@ public class ItemCursorAdapter extends CursorAdapter {
         int position = cursor.getPosition();
         final long id = getItemId(position);
 
+        Button saleButton = (Button) view.findViewById(R.id.sale_button);
+
+        saleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sellProduct(cursor, context, mQuantity, id);
+
+            }
+        });
+
         view.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Toast.makeText(context,"test click", Toast.LENGTH_LONG).show();
+                Toast.makeText(context, "test click", Toast.LENGTH_LONG).show();
 
                 Intent intent = new Intent(context, DetailActivity.class);
 
-                Uri uriCurrentItem = ContentUris.withAppendedId(ItemContract.ItemEntry.CONTENT_URI,id);
+                Uri uriCurrentItem = ContentUris.withAppendedId(ItemContract.ItemEntry.CONTENT_URI, id);
 
                 intent.setData(uriCurrentItem);
 
@@ -85,37 +84,29 @@ public class ItemCursorAdapter extends CursorAdapter {
         });
     }
 
-    public void sellProduct(Cursor cursor, Context context){
+    public void sellProduct(Cursor cursor, Context context, String mQuantity2, long id2) {
 
-       int mQuantity = cursor.getColumnIndexOrThrow(ItemContract.ItemEntry.QUANTITY);
+        int mQuantity = Integer.parseInt(mQuantity2);
 
-       if(mQuantity>0) {
-           mQuantity = mQuantity - 1;
-       }else {
+        if (mQuantity > 0) {
+            mQuantity = mQuantity - 1;
 
-           Toast.makeText(context, "There's no stock of the product", Toast.LENGTH_LONG).show();
-       }
+            Uri uriCurrentItem = ContentUris.withAppendedId(ItemContract.ItemEntry.CONTENT_URI, id2);
 
-        int position = cursor.getPosition();
-        final long id = getItemId(position);
+            //set values from cursor into the content values
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(ItemContract.ItemEntry.QUANTITY, mQuantity);
 
-        Uri uriCurrentItem = ContentUris.withAppendedId(ItemContract.ItemEntry.CONTENT_URI,id);
 
-        //get values from cursor an save in variables
-        String mName = cursor.getString(cursor.getColumnIndexOrThrow(ItemContract.ItemEntry.PRODUCT_NAME));
-        String mPrice = String.valueOf(cursor.getInt(cursor.getColumnIndexOrThrow(ItemContract.ItemEntry.PRICE)));
-        String mMail = String.valueOf(cursor.getInt(cursor.getColumnIndexOrThrow(ItemContract.ItemEntry.MAIL)));
+            int rowsUpdated = context.getContentResolver().update(uriCurrentItem, contentValues, null, null);
 
-        //set values from cursor into the content values
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(ItemContract.ItemEntry.PRODUCT_NAME,mName);
-        contentValues.put(ItemContract.ItemEntry.PRICE,mPrice);
-        contentValues.put(ItemContract.ItemEntry.QUANTITY, mQuantity);
-        contentValues.put(ItemContract.ItemEntry.MAIL, mMail);
+            Toast.makeText(context, "Rows updated: " + rowsUpdated, Toast.LENGTH_LONG).show();
 
-        int rowsUpdated = context.getContentResolver().update(uriCurrentItem,contentValues,null,null);
+        } else {
 
-        Toast.makeText(context,"Rows updated: " + rowsUpdated, Toast.LENGTH_LONG);
+            Toast.makeText(context, "There's no stock of the product", Toast.LENGTH_LONG).show();
+        }
+
 
     }
 
